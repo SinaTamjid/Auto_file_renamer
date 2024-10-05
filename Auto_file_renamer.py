e43f96cc403c0106e2958d1f3bbc1a20d2f2ce1e
SinaTamjid <sinatamjidfts1234@gmail.com>
2024-10-05 11:42:32 +0330

    complete

--- Auto_file_renamer.py
+++ Auto_file_renamer.py
@@ -2,10 +2,10 @@ import os
 import re
 from pathlib import Path
 
-# Path to the directory containing the images
-data_dir = r"C:\Users\Lion\Desktop\images\char-5-epoch-6\train"
-# Path to the directory where renamed images will be saved
-output_dir = r"C:\Users\Lion\Desktop\images\renamed"
+# Path to the directory containing the files
+data_dir = r"your input folder that contains the files"
+# Path to the directory where renamed files will be saved
+output_dir = r"your output directory folder"
 
 # Create the output directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
@@ -13,20 +13,21 @@ os.makedirs(output_dir, exist_ok=True)
 # Get list of all the images
 images = sorted(list(map(str, list(Path(data_dir).glob("*.png")))))
 
-# Regular expression to match the first 5 digits followed by any characters
-pattern = re.compile(r"^\d{5}")
+# Regular expression to match the pattern name you wanna recognize or change
+pattern = re.compile(r"??????")
 
 for image in images:
     # Get the file name
     file_name = os.path.basename(image)
     print(f"Processing file: {file_name}")  # Debugging statement
-    # Find the first 5 digits at the beginning of the file name
+    # Find the pattern from regular expression
     match = pattern.match(file_name)
     if match:
-        # Get the number
+        # Get the match
         number = match.group(0)
         # Create the new file name
-        new_file_name = f"{number}.png"
+        # its could be number letters or anything you want
+        new_file_name = f"{number}.with the format you want"
         new_file_path = os.path.join(output_dir, new_file_name)
         
         # Check if the new file name already exists
